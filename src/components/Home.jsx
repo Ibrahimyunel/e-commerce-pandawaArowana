351ecfdf80cd0d56a339ac8c47d3432753345bd1
Ibrahimyunel <ibrahim.cbshp@gmail.com>
2023-03-13 16:31:14 +0700

    add api for save and retrieve image of upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,16 +3,11 @@ import axios from "../reusable/axios";
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
-const images = [
-    { id: 1, src: '/images/logopandawaarowanaT.png', title: 'foo', alt: 'bar' },
-    { id: 2, src: '/images/logopandawaarowanaT.png', title: 'foo', alt: 'bar' },
-    { id: 3, src: '/images/logopandawaarowanaT.png', title: 'foo', alt: 'bar' },
-    { id: 4, src: '/images/logopandawaarowanaT.png', title: 'foo', alt: 'bar' }
-];
+const getUploadResponse = [];
 
 const Home = () => {
     const [mainVideo, setMainVideo] = useState(false);
-    const [iamgeUpload, setImageUpload] = useState('');
+    const [uploadList, setUploadList] = useState('');
 
     useEffect(() => {
         const authCookie = cookies.get('auth');
@@ -33,17 +28,26 @@ const Home = () => {
 
     async function getImageUpload() {
         try {
-            const res = await axios.post('/image/latest');
-            console.log(res.data);
-            setImageUpload([res.data]);
-        } catch(err) {
-
+            const res = await axios.post('/get-upload');
+            res.data.getUpload.map((item) => {
+                getUploadResponse.push(item)
+            });
+        } catch (err) {
+            console.log(err);
         }
     }
 
-    const iamgeUploadList = iamgeUpload.map(({ id, src, title, alt }) =>
-        <img className="img-fluid" key={id} src={process.env.PUBLIC_URL + src} title={title} alt={alt} />
-    );
+    async function processGetImageUpload() {
+        await getImageUpload();
+        const createImgElements = getUploadResponse.map(({ _id, name }) =>
+            <img className="img-fluid" key={_id} src={"http://localhost:9000/images/upload/" + name} alt={name} />
+        );
+        setUploadList(createImgElements);
+    }
+
+    useEffect(() => {
+        processGetImageUpload();
+    }, []);
 
     return (
         <div className="home">
@@ -56,10 +60,10 @@ const Home = () => {
             <div className="container-md">
                 <div className="row mx-1 mx-md-0">
                     <div className="col-md-6">
-                        {iamgeUploadList}
+                        {uploadList}
                     </div>
                     <div className="col-md-6">
-                        {iamgeUploadList}
+                        {uploadList}
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import './style/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
